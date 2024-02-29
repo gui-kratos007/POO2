3e4aacbe54ef83212d27d0d3fe6af8d0c115cbcc
Guilherme Souza Oliveira <111578950+gui-kratos007@users.noreply.github.com>
2024-02-29 08:43:56 -0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/AgendaTelefonica.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class AgendaTelefonica
+    {
+        public string proprietario { get; set; }
+        public List<Contato> contatos { get; set; }
+
+
+            public AgendaTelefonica(string proprietario)
+        {
+            this.proprietario = proprietario;
+            contatos = new List<Contato>();
+        }
+
+
+        public void inserir(string nome, string numero)
+        {
+            contatos.Add(new Contato(nome, numero));
+        }
+
+
+        public void remover(string nome)
+        {
+            Contato c = buscar(nome);
+            if (contatos.Remove(c))
+            {
+                Console.WriteLine("Não existe esse contato. ");
+            }
+        }
+
+
+        public Contato buscar(string nome)
+        {
+            foreach (Contato c in contatos)
+            {
+                if (c.Nome.Contains(nome))
+                {
+                    return c;
+                }
+            }
+            return null;
+        }
+
+        public int tamanho()
+        {
+            return contatos.Count;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Contato.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Contato
+    {
+        public string Nome{get; set;}
+        public string Numero { get; set;}
+
+
+        public Contato (string nome, string numero)
+        {
+            Nome = nome;
+            Numero = numero;
+        }
+    }
+}
